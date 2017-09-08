5414744c30d346795947caf5fa8eddedf05d8c50
Ruoran Liu <rrliu@thoughtworks.com>
2017-09-08 15:44:49 +0800

    remove lean cloud config

--- server.js
+++ server.js
@@ -4,17 +4,11 @@ var express = require('express'),
   socketIo = require('socket.io'),
   AV = require('leanengine')
 
-AV.init({
-  appId: process.env.LEANCLOUD_APP_ID || 's0F62s1DFX5SmNG2vjmsnsHX-gzGzoHsz',
-  appKey: process.env.LEANCLOUD_APP_KEY || 'XHbKCGvAie5ynb4iQofnJjS8',
-  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY || 'Vfwd96VoboBoLE1FmeEV1dER'
-})
-app.use(AV.express())
 
 app.use(express.static(__dirname + '/public'))
 
 const server = http.createServer(app)
-server.listen((process.env.LEANCLOUD_APP_PORT || 3000), () => {
+server.listen(3000, () => {
   console.log('Server running at :3000')
 })
 const io = socketIo.listen(server)
