ab88ccd01e5fadc48d203c00e2e1e7760038d10e
Xingxin Zeng <star_yes@qq.com>
2018-01-31 21:10:39 +0800

    Save image/like/dislike in server

--- public/save-image.js
+++ public/save-image.js
@@ -1,60 +1,75 @@
-const canvas = document.getElementById('drawing');
+(function() {
+  const socket = io.connect();
 
-const $saveImageTemplate = $('.save-image-template');
-const $sideImages = $('.side-images');
-const $sideImagesContent = $('.side-images-content');
+  const canvas = document.getElementById('drawing');
 
-const $showSide = $('.show-side');
-const $hideSide = $('.hide-side');
+  const $saveImageTemplate = $('.save-image-template');
+  const $sideImages = $('.side-images');
+  const $sideImagesContent = $('.side-images-content');
 
-const $save = $('#save');
+  const $showSide = $('.show-side');
+  const $hideSide = $('.hide-side');
 
-const likeImage = $image => {
-  const $likeNumberElement = $image.find('.like-number');
-  let likeNumber = parseInt($likeNumberElement.text());
-  likeNumber++;
-  $likeNumberElement.text(likeNumber);
-};
+  const $save = $('#save');
 
-const dislikeImage = $image => {
-  const $dislikeNumberElement = $image.find('.dislike-number');
-  let likeNumber = parseInt($dislikeNumberElement.text());
-  likeNumber++;
-  $dislikeNumberElement.text(likeNumber);
-};
+  const addEvent = $image => {
+    $image.find('.like').click(() => {
+      socket.emit('like', $image.attr('id'));
+    });
 
-const addEvent = $image => {
-  $image.find('.like').click(() => {
+    $image.find('.dislike').click(() => {
+      socket.emit('dislike', $image.attr('id'));
+    });
+  };
+
+  $save.click(() => {
+    const imageUrl = canvas.toDataURL();
+    socket.emit('saveImage', imageUrl);
+  });
+
+  const likeImage = $image => {
+    const $likeNumberElement = $image.find('.like-number');
+    let likeNumber = parseInt($likeNumberElement.text());
+    likeNumber++;
+    $likeNumberElement.text(likeNumber);
+  };
+
+  const dislikeImage = $image => {
+    const $dislikeNumberElement = $image.find('.dislike-number');
+    let likeNumber = parseInt($dislikeNumberElement.text());
+    likeNumber++;
+    $dislikeNumberElement.text(likeNumber);
+  };
+
+  socket.on('like', image => {
+    const $image = $(`#${image.id}`);
     likeImage($image);
   });
 
-  $image.find('.dislike').click(() => {
+  socket.on('dislike', image => {
+    const $image = $(`#${image.id}`);
     dislikeImage($image);
   });
-};
 
-$save.click(() => {
-  const imageUrl = canvas.toDataURL();
+  socket.on('saveImage', image => {
+    const $saveImage = $saveImageTemplate.clone();
+    $saveImage.find('img').attr('src', image.url);
+    $saveImage.attr('id', image.id);
+    $saveImage.find('.like-number').text(image.like);
+    $saveImage.find('.dislike-number').text(image.dislike);
+    $sideImagesContent.append($saveImage);
+    addEvent($saveImage);
+  });
 
-  const $saveImage = $saveImageTemplate.clone();
+  $showSide.click(() => {
+    $sideImages.addClass('show');
+    $showSide.hide();
+    $hideSide.show();
+  });
 
-  $saveImage.find('img').attr('src', imageUrl);
-
-  $sideImagesContent.append($saveImage);
-  // save.href = canvas.toDataURL();
-  // save.download = 'image.png';
-
-  addEvent($saveImage);
-});
-
-$showSide.click(() => {
-  $sideImages.addClass('show');
-  $showSide.hide();
-  $hideSide.show();
-});
-
-$hideSide.click(() => {
-  $sideImages.removeClass('show');
-  $showSide.show();
-  $hideSide.hide();
-});
+  $hideSide.click(() => {
+    $sideImages.removeClass('show');
+    $showSide.show();
+    $hideSide.hide();
+  });
+})();
--- server.js
+++ server.js
@@ -3,39 +3,81 @@ var express = require('express'),
   http = require('http'),
   socketIo = require('socket.io');
 
+app.use(express.static(__dirname + '/public'));
 
-app.use(express.static(__dirname + '/public'))
-
-const server = http.createServer(app)
+const server = http.createServer(app);
 server.listen(3000, () => {
-  console.log('Server running at :3000')
-})
-const io = socketIo.listen(server)
+  console.log('Server running at :3000');
+});
+const io = socketIo.listen(server);
 
-let lineHistory = []
+let lineHistory = [];
+let images = [];
+let currentImageId = 0;
 
 io.on('connection', socket => {
   lineHistory.forEach(data => {
-    socket.emit('drawLine', data)
-  })
+    socket.emit('drawLine', data);
+  });
+
   socket.on('drawLine', data => {
-    lineHistory.push(data)
-    io.emit('drawLine', data)
-  })
+    lineHistory.push(data);
+    io.emit('drawLine', data);
+  });
   socket.on('clear', () => {
-    lineHistory = []
-    io.emit('clear')
-  })
+    lineHistory = [];
+    io.emit('clear');
+  });
   socket.on('login', name => {
-    io.emit('chat', 'Bot', `Welcome ${name} to join`)
-  })
+    io.emit('chat', 'Bot', `Welcome ${name} to join`);
+  });
 
   socket.on('chat', (username, message) => {
-    if (message.startsWith('http') &&
-      (message.endsWith('jpg') || message.endsWith('png'))) {
-        io.emit('drawImage', message)
+    if (
+      message.startsWith('http') &&
+      (message.endsWith('jpg') || message.endsWith('png'))
+    ) {
+      io.emit('drawImage', message);
     } else {
-      io.emit('chat', username, message)
+      io.emit('chat', username, message);
     }
-  })
-})
+  });
+
+  images.forEach(image => {
+    socket.emit('saveImage', image);
+  });
+
+  socket.on('saveImage', imageUrl => {
+    const image = {
+      id: `image_${currentImageId}`,
+      url: imageUrl,
+      like: 0,
+      dislike: 0,
+    };
+
+    images.push(image);
+
+    currentImageId++;
+    io.emit('saveImage', image);
+  });
+
+  socket.on('like', id => {
+    const index = images.findIndex(image => image.id === id);
+    if (index >= 0) {
+      const image = images[index];
+
+      image.like = image.like + 1;
+      io.emit('like', image);
+    }
+  });
+
+  socket.on('dislike', id => {
+    const index = images.findIndex(image => image.id === id);
+    if (index >= 0) {
+      const image = images[index];
+
+      image.dislike = image.dislike + 1;
+      io.emit('dislike', image);
+    }
+  });
+});
