bdca7f0ff79b4c8421ee0c5c000fb9d04adf5cc4
Xingxin Zeng <star_yes@qq.com>
2018-03-11 22:25:41 +0800

    Change title and change deploy script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,16 +3,18 @@ module.exports = {
    * Application configuration section
    * http://pm2.keymetrics.io/docs/usage/application-declaration/
    */
-  apps: [{
-    name: 'Draw-n-guess',
-    script: './server.js',
-    env: {
-      COMMON_VARIABLE: 'true'
+  apps: [
+    {
+      name: 'Draw-n-guess',
+      script: './server.js',
+      env: {
+        COMMON_VARIABLE: 'true',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
     },
-    env_production: {
-      NODE_ENV: 'production'
-    }
-  }],
+  ],
 
   /**
    * Deployment section
@@ -24,17 +26,19 @@ module.exports = {
       host: 'ec2-13-229-88-236.ap-southeast-1.compute.amazonaws.com',
       key: '~/.ssh/AWS.pem',
       ref: 'origin/master',
-      repo: 'git@github.com:lrrfantasy/draw-n-guess.git',
-      path: '/var/www/production',
-      'post-deploy': 'yarn install && pm2 reload ecosystem.config.js --env production'
+      repo: 'git@github.com:SBeator/draw-n-guess.git',
+      path: '/var/www/draw-n-guess',
+      'post-deploy':
+        'yarn install && pm2 reload ecosystem.config.js --env production',
     },
     'local-deploy': {
       user: 'ec2-user',
       host: 'localhost',
       ref: 'origin/master',
-      repo: 'git@github.com:lrrfantasy/draw-n-guess.git',
-      path: '/var/www/production',
-      'post-deploy': 'yarn install && pm2 reload ecosystem.config.js --env production'
-    }
-  }
+      repo: 'git@github.com:SBeator/draw-n-guess.git',
+      path: '/var/www/draw-n-guess',
+      'post-deploy':
+        'yarn install && pm2 reload ecosystem.config.js --env production',
+    },
+  },
 };
